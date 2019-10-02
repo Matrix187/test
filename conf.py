9c1c6a9abc7588e8164eb5f02adb36d49bae3fd5
bursh@il.ibm.com <Treanon187lil>
2019-10-02 11:32:35 +0300

    666

--- conf.py
+++ conf.py
@@ -31,6 +31,7 @@ author = 'Bursh'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = []
+master_doc = 'index'
 
 extensions = ['sphinx.ext.autodoc','sphinx.ext.viewcode']
 # Add any paths that contain templates here, relative to this directory.
